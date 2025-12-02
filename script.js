5f3d243e1f884c2b21c25bd0f6a941f08c2bff7d
F4lk0nR4ng3r <ppetrusianlt@gmail.com>
2025-12-03 01:33:02 +0200

    Galbūt užbaigiau tą 60%

--- script.js
+++ script.js
@@ -1,46 +1,18 @@
-/*
-filterSelection("all");
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+function addToCartFromHTML(button) {
+  var itemDiv = button.parentElement;
+  var name = itemDiv.querySelector(".car-name").innerText;
+  var img = itemDiv.querySelector("img").src;
 
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
+  var cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
+  var existing = cart.find((item) => item.name === name);
 
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+  if (existing) {
+    existing.quantity += 1;
+  } else {
+    cart.push({ name: name, image: img, quantity: 1 });
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(name + " pridėta į krepšelį!");
 }
-*/
-// ^^^ Ne mano sukurtas kodas, tai negalėčiau apginti projektinį darbą. ^^^ 
