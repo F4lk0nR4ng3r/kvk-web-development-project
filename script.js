e39657a840cb74d16d2a3e17f6d7a573b3d366a4
F4lk0nR4ng3r <ppetrusianlt@gmail.com>
2025-12-03 12:15:02 +0200

    beveik

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
+var cart = [];
 function addToCartFromHTML(button) {
   var itemDiv = button.parentElement;
   var name = itemDiv.querySelector(".car-name").innerText;
   var img = itemDiv.querySelector("img").src;
-
-  var cart = JSON.parse(localStorage.getItem("cart")) || [];
-
   var existing = cart.find((item) => item.name === name);
-
   if (existing) {
     existing.quantity += 1;
   } else {
     cart.push({ name: name, image: img, quantity: 1 });
   }
-
-  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartCount();
   alert(name + " pridėta į krepšelį!");
+  console.log(cart);
 }
